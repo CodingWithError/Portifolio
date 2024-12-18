1c46c6b0e7eac246057a41e5ac418229adaed7d8
CodingWithError <vshravanth2004@gmail.com>
2024-12-19 01:53:11 +0530

    Portfolio

---
+++ js/commands.js
@@ -0,0 +1,108 @@
+var twitter = "https://x.com/V_Shravanth2004";
+var linkedin = "https://www.linkedin.com/in/v-shravanth-43b469274/";
+var github = "https://github.com/CodingWithError";
+var email = "mailto:vshravanth2004@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Shravanth!👋</span>",
+  "<br>",
+  "<li>🎓 I'm currently pursuing a Bachelors + Masters of Science in Computer Science at Scaler School of Technology (2023–2027), and a Bachelor of Science in Computer Science at BITS (2023–2026).</li>",
+  "<li>💻 My technical expertise includes:</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Languages: Java, JavaScript, Python
+        - Frameworks/Technologies: React, Next.js, SpringBoot, MongoDB, Express.js, SQL
+        - Tools: Git, Shell Scripting, Figma</pre>`,
+  `<li><pre class="whitespace-pre-wrap">🚀 I enjoy building projects that solve real-world problems.
+   I have a passion for full-stack development and designing user-friendly interfaces,
+   while continuously improving my skills and learning new technologies.</pre></li>`,
+  `<li>📫 Feel free to contact me at <a href="mailto:vshravanth2004@gmail.com">vshravanth2004@gmail.com</a>.</li>`,
+  "<br>",
+];
+
+
+social = [
+  "<br>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">twitter/V Shravanth' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">linkedin/V Shravanth' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/CodingWithEroor' +
+    "</a>",
+  "<br>",
+];
+
+
+projects = [
+  "<br>",
+  `<a href="https://github.com/CodingWithError/Friend_Me" target="_blank">
+<span class="underline">Friend Me</span>
+</a>
+<br><br>
+<pre class="indent-8 whitespace-pre-wrap">
+Friend Me is a web app designed to interact with you as your best friend. It uses basic HTML, CSS, and JavaScript, along with the Gemini API, to create a friendly and engaging conversational experience.
+
+This app goes beyond simple conversations by understanding and reacting to the user's feelings. It can adapt its responses based on emotions, providing uplifting messages when you're feeling down, celebrating with you in happy moments, and offering a listening ear when you need it most. Friend Me is your digital companion, designed to make you feel heard and supported.
+
+Key Features:
+    * Friendly and engaging conversational interface.
+    * Emotion-aware responses to react appropriately to the user's feelings.
+    * Provides motivational and comforting messages when needed.
+    * Easy-to-use and lightweight design.
+
+Built With:
+    * HTML
+    * CSS
+    * JavaScript
+    * Gemini API
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/CodingWithError/Web-Scraping-python" target="_blank">
+<span class="underline">Web Scraping Python</span>
+</a>
+<br><br>
+<pre class="indent-8 whitespace-pre-wrap">
+Web Scraping Python is a script designed to scrape data from online stores like Amazon, Flipkart, and Snapdeal. Built using Python and Beautiful Soup, this scraper fetches product prices from multiple websites and displays them in the terminal, allowing for easy price comparison.
+
+Key Features:
+    * Scrapes product data from Amazon, Flipkart, and Snapdeal.
+    * Displays product prices in the terminal for quick comparisons.
+    * Built with Python for robust and efficient scraping.
+    * Uses Beautiful Soup for parsing HTML content.
+
+Built With:
+    * Python
+    * Beautiful Soup
+</pre>`,
+
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
